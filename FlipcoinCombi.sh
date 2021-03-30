6e5a10916ddaf823febb29cc5db4946b45676692
sohamdhamnaskar <sohamdham123@gmail.com>
2021-03-31 02:16:55 +0530

    Added: Multiple flipping of singlet.

--- FlipcoinCombi.sh
+++ FlipcoinCombi.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -x
 
-flip=$((RANDOM%2))
+H=0
+T=0
 
+declare -A coinflip
+
+for (( i=1; i<=10; i++ ))
+do
+			flip=$((RANDOM%2))
 	if [ $flip -eq 1 ]
-	then
-		echo Heads
-	else
-		echo Tails
+
+		   then coinflip[Heads]=$((++H))
+
+			else coinflip[Tails]=$((++T))
 	fi
+done
+
+perhead=$(( $(( $H*10)) ))
+pertail=$(( $(( $T*10)) ))
+
+echo "Heads= $H"
+echo "Percentage of Heads is= $perhead"
+echo "Tails= $T"
+echo "Percentage of Tails is= $pertail"
